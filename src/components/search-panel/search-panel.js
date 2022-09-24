4ca1e850b2df9e8678c05e6d418b42b11f341573
Dmitriy <Dmitriy.Terekh@gmail.com>
2022-09-24 19:07:07 +0300

    v1.0

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,33 @@
+import { Component } from 'react';
+
+import './search-panel.css';
+
+class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: '',
+            filter: ''
+        }
+    }
+
+    onUpdateSearch = (e) => {
+        const term = e.target.value;
+        this.setState({term});
+        this.props.onUpdateSearch(term)
+    }
+
+    render() {
+        return (
+            <input 
+                placeholder='Найти сотрудника'
+                type='text' 
+                className='form-control search-input'
+                value={this.state.term}
+                onChange={this.onUpdateSearch}>
+            </input>
+        )
+    }
+}
+
+export default SearchPanel;
